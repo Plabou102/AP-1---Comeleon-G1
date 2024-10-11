048e79b73cab3eb11b622ff1036bb4a28e16ecb8
Tiago Duval <tiago.duval@stadjutor.com>
2024-10-11 15:05:05 +0200

    Feature/(page)création de la page avis

--- symfony/slam_travel_site/src/Controller/AvisController.php
+++ symfony/slam_travel_site/src/Controller/AvisController.php
@@ -38,9 +38,12 @@ class AvisController extends AbstractController
 
         return $this->render('avis/index.html.twig', [
             'controller_name' => 'AvisController',
+            'page_title' => 'Avis'
         ]);
     }
 }
+
+
 
 
 
