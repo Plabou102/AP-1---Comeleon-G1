af1ff1e9a0edb8152937d7e2ea32f44356178f36
Plabou <adaimebou@gmail.com>
2024-10-18 16:47:23 +0200

    <dev>(readme):readme

---
+++ AP-1---Comeleon-G1-dev/symfony/slam_travel_site/migrations/Version20241018143332.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20241018143332 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE contact (id INT AUTO_INCREMENT NOT NULL, iduser INT DEFAULT NULL, nom VARCHAR(255) NOT NULL, prenom VARCHAR(255) NOT NULL, motif VARCHAR(255) NOT NULL, date DATETIME NOT NULL, INDEX IDX_4C62E6385E5C27E9 (iduser), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE contact ADD CONSTRAINT FK_4C62E6385E5C27E9 FOREIGN KEY (iduser) REFERENCES utilisateur (id) ON DELETE CASCADE');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE contact DROP FOREIGN KEY FK_4C62E6385E5C27E9');
+        $this->addSql('DROP TABLE contact');
+    }
+}
