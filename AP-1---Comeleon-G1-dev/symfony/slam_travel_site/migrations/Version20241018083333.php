ef0517aa8ad300555e866bc44ab483793a6a80cd
Plabou <adaimebou@gmail.com>
2024-10-18 15:09:53 +0200

    <Feature>(page):Prestations

--- AP-1---Comeleon-G1-dev/symfony/slam_travel_site/migrations/Version20241018083333.php
+++ AP-1---Comeleon-G1-dev/symfony/slam_travel_site/migrations/Version20241018083333.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20241011151132 extends AbstractMigration
+final class Version20241018083333 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20241011151132 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE utilisateur (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(255) NOT NULL, prenom VARCHAR(255) NOT NULL, username VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, roles JSON NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_IDENTIFIER_USERNAME ON utilisateur (username)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE utilisateur');
+        $this->addSql('DROP INDEX UNIQ_IDENTIFIER_USERNAME ON utilisateur');
     }
 }
